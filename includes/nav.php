ae03c4c9d604aa2b76ffb37268db09d89c5ba17e
Noel Picinich <np@tsl.io>
2017-10-31 21:35:44 -0400

    Remove PHP

--- includes/nav.php
+++
@@ -1,8 +0,0 @@
-<nav>
-  <ul class="nav">
-    <li class="nav-item"><a class="nav-link button" href="index.php">Home</a></li>
-    <li class="nav-item"><a class="nav-link button" href="about.php">About</a></li>
-    <li class="nav-item"><a class="nav-link button" href="projects.php">Projects</a></li>
-    <li class="nav-item"><a class="nav-link button" href="contact.php">Contact</a></li>
-  </ul>
-</nav>
