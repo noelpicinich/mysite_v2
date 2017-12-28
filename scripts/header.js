ffc85f33bafe729a75bc388031033953cadc6964
Noel Picinich <np@tsl.io>
2017-12-28 12:00:40 -0500

    New scroll indicators and header edits

--- scripts/header.js
+++ scripts/header.js
@@ -7,6 +7,52 @@ $(document).ready(function() {
 
   $(window).resize( function() {
     $("header").css("height", $(window).height());
-  })
+  });
 
-})
+  var home_top = $('#home').offset().top;
+  var about_top = $('#about').offset().top;
+  var experience_top = $('#experience').offset().top;
+  var campus_top = $('#campus').offset().top;
+  var projects_top = $('#projects').offset().top;
+
+  var current_active = '#home_item';
+
+  $(window).scroll(function (event) {
+    var scroll = $(window).scrollTop();
+
+    if (scroll > campus_top + height/2) {
+      $('#projects_item').addClass('active');
+      if (current_active != '#projects_item') {
+        $(current_active).removeClass('active');
+      };
+      current_active = '#projects_item';
+    } else if (scroll > experience_top + height/2) {
+      $('#campus_item').addClass('active');
+      if (current_active != '#campus_item') {
+        $(current_active).removeClass('active');
+      };
+      current_active = '#campus_item';
+    } else if (scroll > about_top + height/2) {
+      $('#experience_item').addClass('active');
+      if (current_active != '#experience_item') {
+        $(current_active).removeClass('active');
+      };
+      current_active = '#experience_item';
+    } else if (scroll > home_top + height/2) {
+      $('#about_item').addClass('active');
+      if (current_active != '#about_item') {
+        $(current_active).removeClass('active');
+      };
+      current_active = '#about_item';
+    } else if (scroll < home_top + height/2) {
+      $('#home_item').addClass('active');
+      if (current_active != '#home_item') {
+        $(current_active).removeClass('active');
+      };
+      current_active = '#home_item';
+    }
+
+
+  });
+
+});
